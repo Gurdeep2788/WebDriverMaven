91b2b5a3a90c27c2f8ff6c8bb9fd0922221a7075
Gurdeep Singh <gurdeep.singh@nagarro.com>
2019-06-09 01:20:22 +0530

    Iitial Commit

---
+++ src/test/java/webdriverMaven/LoginTest.java
@@ -0,0 +1,41 @@
+package webdriverMaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	WebDriver driver=null;
+	
+	@BeforeSuite
+	public void setUp(){
+	System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\src\\main\\resources\\executables\\chromedriver.exe");
+	driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	}
+	
+	
+	@Test
+	public void gmailLogin() throws InterruptedException{		
+		
+		driver.get("https://gmail.com");
+		driver.findElement(By.id("identifierId")).sendKeys("gurukohliopp@gmail.com");
+		driver.findElement(By.xpath("//span[contains(text(),'Next')]")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Mskohli@2");
+		driver.findElement(By.xpath("//span[contains(text(),'Next')]")).click();
+		Thread.sleep(3000);
+			
+	} 	
+	
+	@AfterSuite
+	public void tearDown()
+	{
+		driver.quit();
+	}
+
+}
